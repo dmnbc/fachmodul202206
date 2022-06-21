350ddac4aa05e1dcbf5d0808b0b1de64db2680ec
dmnbc <31969318+dmnbc@users.noreply.github.com>
2022-06-21 14:22:08 +0200

    Bisheriger Code zum Schachspiel
    Die Datei enthält einige Basisfunktionen für ein Schachspiel

---
+++ chessMusterGrundstellungZeigen.cpp
@@ -0,0 +1,218 @@
+// chessMuster.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <iostream> // ein und ausgabe methoden laden 
+#include <array>
+char hell   = 219;
+char dunkel = 176;
+int ausdehnung = 10;
+int zeile;
+int spalte;
+std::array<std::array<char, 10>, 10>   spielfeld; 
+int vonSpalte, vonZeile, nachSpalte, nachZeile;
+
+void spielstand_anzeigen()
+{
+	std::cout << "Der aktuelle Spielstand\n\n";
+	for (std::array<char,10> zeile : spielfeld)
+	{
+		for (char spalte : zeile)
+		{
+			std::cout << spalte;
+		}
+		std::cout << std::endl;
+	}
+}
+
+void grundstellung()
+{
+	for (zeile = 0; zeile < ausdehnung; zeile++)
+	{
+		for (spalte = 0; spalte < ausdehnung; spalte++)
+		{
+			switch (zeile) // if ( zeile == 0 || zeile == 9 )
+			{
+			case 0: case 9: // oberste oder letzte Zeile mit Umrandung
+				if ((spalte) == 0 || (spalte) == 9)
+				{
+					spielfeld[zeile][spalte] = '*';
+					// std::cout << '*';   // die vier Ecken 
+				}
+				else
+				{
+					spielfeld[zeile][spalte] = char(spalte + 64); // ASCII Buchstaben ab 64 
+				}
+				break;
+			case 2: case 7:  // Zeilen mit Bauern füllen
+				if (spalte > 0 && spalte < 9)
+				{				
+						spielfeld[zeile][spalte] = ((zeile % 2 == 0)?'B': 'b');			
+				}
+				else
+				{
+					spielfeld[zeile][spalte] = char(zeile + 48);
+				}
+				break;
+			case 1: case 8:
+				switch (spalte)
+				{
+				case 1: case 8:
+					if (zeile % 2 != 0)
+					{
+						spielfeld[zeile][spalte] = 'T';
+					}
+					else
+					{
+						spielfeld[zeile][spalte] = 't';
+					}
+					break;
+				case 2: case 7:
+					if (zeile % 2 != 0)
+					{
+						spielfeld[zeile][spalte] = 'P';
+					}
+					else
+					{
+						spielfeld[zeile][spalte] = 'p';
+					}
+					break;
+				case 3: case 6:
+					if (zeile % 2 != 0)
+					{
+						spielfeld[zeile][spalte] = 'L';
+					}
+					else
+					{
+						spielfeld[zeile][spalte] = 'l';
+					}
+					break;
+				case 4:
+					if (zeile % 2 != 0)
+					{
+						spielfeld[zeile][spalte] = 'D';
+					}
+					else
+					{
+						spielfeld[zeile][spalte] = 'd';
+					}
+					break;
+				case 5:
+					if (zeile % 2 != 0)
+					{
+						spielfeld[zeile][spalte] = 'K';
+					}
+					else
+					{
+						spielfeld[zeile][spalte] = 'k';
+					}
+					break;
+
+				default:
+					spielfeld[zeile][spalte] = char(zeile + 48);
+				}
+
+				break;
+			default:              // auf dem Spielfeld Zeile 1 bis 8
+				switch (spalte)
+				{
+				case 0: case 9:   // erste oder letzte Spalte mit Ziffern
+					spielfeld[zeile][spalte] = char(zeile + 48); // ASCII Ziffern ab 48
+					break;
+				default:
+					if ((zeile + spalte) % 2) // 
+						spielfeld[zeile][spalte] = hell;
+					else
+						spielfeld[zeile][spalte] = dunkel;
+				}
+			}
+		}
+		// std::cout << std::endl;
+	}
+}
+
+bool zugErlaubt()
+{
+	bool ok = false;
+	// ist der Zug noch auf dem Spielfeld -> indizes müssen zwischen 1 und 8 liegen
+	if (vonZeile >= 1 && vonZeile <= 8
+		&& vonSpalte >= 1 && vonSpalte <= 8
+		&& nachZeile >= 1 && nachZeile <= 8
+		&& nachSpalte >= 1 && nachSpalte <= 8)
+	{
+		ok = true;
+	}
+
+	if (spielfeld[vonZeile][vonSpalte] == dunkel 
+	|| spielfeld[vonZeile][vonSpalte]  == hell)
+	{
+		ok = false;
+	}
+
+	return ok;
+}
+
+void userEingabe(std::string vonWo, std::string nachWo)
+{
+	std::cout << __LINE__ << "\n";
+	std::cout << "Es wird der Zug " << vonWo << " nach " << nachWo << " berechnet\n";
+	vonZeile =    vonWo[1] - 48;
+	vonSpalte=    toupper(vonWo[0]) - 64;  // Umrechnung von ASCII in int
+	nachZeile =  nachWo[1] - 48;
+	nachSpalte = toupper(nachWo[0]) - 64;
+
+	std::cout << "Es wird der Zug spielfeld["<<vonZeile<<"]["<<vonSpalte << "] nach spielfeld["
+		                 << nachZeile <<"]["<<nachSpalte<<"] berechnet\n";
+	if(zugErlaubt())
+	std::cout << "Bewegt wird die Figur " << spielfeld[vonZeile][vonSpalte] << "\n";
+	
+}
+
+void ersetzFeld(int vonZeile, int vonSpalte)
+{
+	if ((vonZeile + vonSpalte) % 2) 
+		spielfeld[vonZeile][vonSpalte] = hell;
+	else
+		spielfeld[vonZeile][vonSpalte] = dunkel;
+	
+}
+
+void figurBewegen()
+{
+	spielfeld[nachZeile][nachSpalte] = spielfeld[vonZeile][vonSpalte];
+	ersetzFeld(vonZeile, vonSpalte);
+}
+
+
+
+int main()
+{
+	std::cout << __LINE__ << "\n";
+	std::string eingabeVon, eingabeNach;
+
+	grundstellung();
+	spielstand_anzeigen();
+	std::cout << "Bitte geben Sie die Startkoordinaten der Figur an :";
+	std::cin >> eingabeVon;
+	std::cout << "Bitte geben Sie die Zielkoordinaten der Figur an :";
+	std::cin >> eingabeNach;
+	std::cout << __LINE__ << "\n";
+
+
+	userEingabe(eingabeVon, eingabeNach);
+
+	while (!zugErlaubt())
+	{
+		std::cout << "Der Zug ist nicht erlaubt ! Bitte einen regelkonformen Zug eingeben\n";
+		std::cout << "Bitte geben Sie die Startkoordinaten der Figur an :";
+		std::cin >> eingabeVon;
+		std::cout << "Bitte geben Sie die Zielkoordinaten der Figur an :";
+		std::cin >> eingabeNach;
+		userEingabe(eingabeVon, eingabeNach);
+	}   // ein gültiger Zug wurde angefordert
+
+
+	figurBewegen();
+	spielstand_anzeigen();
+
+}
+
